e9c6f91de5bfdf0c979345dd53c3c83d311aaabb
Avahia <39138977+Avahia@users.noreply.github.com>
2018-05-29 23:54:50 -0700

    Mise a Jour

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ScanPageModule } from '../pages/scan/scan.module'
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    ScanPageModule,
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/scan/scan.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult} from '@ionic-native/barcode-scanner';
+import { ToastController } from 'ionic-angular';
+
+/**
+ * Generated class for the ScanPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-scan',
+  templateUrl: 'scan.html',
+})
+export class ScanPage {
+  result: BarcodeScanResult;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private bcs: BarcodeScanner, private toastCtrl: ToastController) {
+  }
+
+  scanBarcode(){
+    const options: BarcodeScannerOptions ={
+      prompt: 'Pointer votre camera vers un Qr Code',
+      torchOn: false
+    };
+
+    this.bcs.scan(options)
+      .then(res =>{
+        this.result = res;
+      })
+      .catch(err => {
+        this.toastCtrl.create({
+          message:err.message
+        }).present ();
+      })
+  }
+}
